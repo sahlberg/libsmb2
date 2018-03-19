3a243653f9a9ebb69f067f17010b7a5099f53f9f
Anton Fedchin <anightik@gmail.com>
2018-03-19 17:27:03 +0300

    pass domain and workstation to ntlmssp_init_context

--- lib/libsmb2.c
+++ lib/libsmb2.c
@@ -564,7 +564,8 @@ negotiate_cb(struct smb2_context *smb2, int status,
 #ifndef HAVE_LIBKRB5
         c_data->auth_data = ntlmssp_init_context(smb2->user,
                                                  smb2->password,
-                                                 NULL, NULL,
+                                                 smb2->domain, 
+                                                 smb2->workstation,
                                                  smb2->client_challenge);
 #else
         c_data->auth_data = krb5_negotiate_reply(smb2, c_data->server,
