sahlberg/libsmb2

commit bf5c12d7072d3a81438ffee67e7308077bfa3adb

make it possible to use alternative username for the share-enum example
Signed-off-by: Ronnie Sahlberg <ronniesahlberg@gmail.com>

lib/init.c
@@ -386,6 +386,8 @@ static void smb2_set_password_from_file(struct smb2_context *smb2)
         char *domain, *user, *password;
         int finished;
 
+        smb2_set_password(smb2, NULL);
+
 #ifdef _MSC_UWP
 // GetEnvironmentVariable is not available for UWP up to 10.0.16299 SDK
 #if defined(NTDDI_WIN10_RS3) && (NTDDI_VERSION >= NTDDI_WIN10_RS3)

examples/smb2-share-enum.c
@@ -100,6 +100,9 @@ int main(int argc, char *argv[])
                         smb2_get_error(smb2));
                 exit(0);
         }
+        if (url->user) {
+                smb2_set_user(smb2, url->user);
+        }
 
         smb2_set_security_mode(smb2, SMB2_NEGOTIATE_SIGNING_ENABLED);
 

include/smb2/libsmb2.h
@@ -90,8 +90,8 @@ typedef int t_socket;
 /*
  * Create an SMB2 context.
  * Function returns
- *  NULL : Failed to create a context.
- *  *nfs : A pointer to an smb2 context.
+ *  NULL  : Failed to create a context.
+ *  *smb2 : A pointer to an smb2 context.
  */
 struct smb2_context *smb2_init_context(void);
 
@@ -306,7 +306,7 @@ struct smb2_pdu;
  * ...
  * *, smb2_queue_pdu(smb2, pdu);
  *
- * See libnfs.c and smb2-raw-stat-async.c for examples on how to use
+ * See libsmb2.c and smb2-raw-stat-async.c for examples on how to use
  * this interface.
  */
 void smb2_add_compound_pdu(struct smb2_context *smb2,

commit 34500e5d56d2fbc1634b81642eab0c8cd1f08d24

don't override password set via smb2_set_password()
Regression from bf5c12d7072d3a81438ffee67e7308077bfa3adb

This was impossible for an user to provide a password via smb2_set_password()
since it was overridden from smb2_connect_share_async(). Indeed,
smb2_set_password_from_file() is called from smb2_set_user(), that is called
from smb2_connect_share_async().

lib/init.c
@@ -386,8 +386,6 @@ static void smb2_set_password_from_file(struct smb2_context *smb2)
         char *domain, *user, *password;
         int finished;
 
-        smb2_set_password(smb2, NULL);
-
 #ifdef _MSC_UWP
 // GetEnvironmentVariable is not available for UWP up to 10.0.16299 SDK
 #if defined(NTDDI_WIN10_RS3) && (NTDDI_VERSION >= NTDDI_WIN10_RS3)
@@ -413,6 +411,9 @@ static void smb2_set_password_from_file(struct smb2_context *smb2)
         if (!fh) {
             return;
         }
+
+        smb2_set_password(smb2, NULL);
+
         while (!feof(fh)) {
                 if (fgets(buf, 256, fh) == NULL) {
                         break;