e0e3567ae0e5eaa2597ba6eb680b94b5848af4a6
Wolf3s <andregui17@outlook.com>
2024-05-12 21:24:39 -0300

    Update compat.h

--- lib/compat.h
+++ lib/compat.h
@@ -521,6 +521,7 @@ struct iovec {
 #define sockaddr_in6 sockaddr_in
 #else
 #include <sys/_iovec.h>
+#define HAVE_SYS__IOVEC_H 1 /* TODO: Move this to a config.h file */
 #endif
 
 #ifndef EAI_AGAIN
