e2d87d4d08857ecc4d63adb49c3ed8efc912428f
bdodge <bdodge09@gmail.com>
2024-09-30 09:23:37 -0400

    uncomment

--- lib/smb2-cmd-tree-disconnect.c
+++ lib/smb2-cmd-tree-disconnect.c
@@ -139,12 +139,10 @@ smb2_cmd_tree_disconnect_reply_async(struct smb2_context *smb2,
                 smb2_free_pdu(smb2, pdu);
                 return NULL;
         }
-        /*
         if (smb2_pad_to_64bit(smb2, &pdu->out) != 0) {
                 smb2_free_pdu(smb2, pdu);
                 return NULL;
         }
-        */
         return pdu;
 }
 
