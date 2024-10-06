b397df45f7f209d315ceb095c0d94b33c49c4f78
Ronnie Sahlberg <ronniesahlberg@gmail.com>
2024-10-06 14:07:22 +1000

    tests: run the socket failure test from 1 to 43
    Signed-off-by: Ronnie Sahlberg <ronniesahlberg@gmail.com>

--- tests/test_0102_ls_basic_socket_error.sh
+++ tests/test_0102_ls_basic_socket_error.sh
@@ -4,8 +4,8 @@
 
 echo "basic ls test with valgrind"
 
-echo -n "Testing prog_ls on root of share with socket failures "
-for IDX in `seq 10 10`; do
+echo "Testing prog_ls on root of share with socket failures "
+for IDX in `seq 1 43`; do
     echo -n "Testing prog_ls on root of share with socket failure at #${IDX} ..."
     READV_CLOSE=${IDX} LD_PRELOAD=./ld_sockerr.so libtool --mode=execute valgrind --leak-check=full --show-leak-kinds=all --error-exitcode=1 ./prog_ls "${TESTURL}/" >/dev/null 2>valgrind.out || failure
     success
