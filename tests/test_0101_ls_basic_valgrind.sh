b6ff171ee9ac9f7cf9776cad8ce028d681039bb3
Ronnie Sahlberg <ronniesahlberg@gmail.com>
2024-10-06 13:01:26 +1000

    tests: add tests to simulate socket failures and check with valgrind for leaks
    The test program prog_ls does leak in this situation so we need to fix that
    and check if it is the test program or libsmb2 that leaks.
    Then we can run tis type of tests on all binaries we build.
    
    Valgrind logs on failure are in tests/valgrind.out
    
    Signed-off-by: Ronnie Sahlberg <ronniesahlberg@gmail.com>

--- tests/test_0101_ls_basic_valgrind.sh
+++ tests/test_0101_ls_basic_valgrind.sh
@@ -2,10 +2,10 @@
 
 . ./functions.sh
 
-echo "basic ls test"
+echo "basic ls test with valgrind"
 
 echo -n "Testing prog_ls on root of share ... "
-libtool --mode=execute valgrind --leak-check=full --error-exitcode=1 ./prog_ls "${TESTURL}/" > /dev/null || failure
+libtool --mode=execute valgrind --leak-check=full --error-exitcode=1 ./prog_ls "${TESTURL}/" >/dev/null 2>&1 || failure
 success
 
 exit 0
