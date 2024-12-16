f0ded5e09aff0dfa96eb323ec1d057f8f17ba9b8
Ronnie Sahlberg <ronniesahlberg@gmail.com>
2024-12-16 15:38:31 +1000

    Add more tests for opening directories that may or may not exist
    Signed-off-by: Ronnie Sahlberg <ronniesahlberg@gmail.com>

--- tests/test_0102_ls_basic_socket_error.sh
+++ tests/test_0102_ls_basic_socket_error.sh
@@ -8,7 +8,8 @@ NUM_CALLS=`libtool --mode=execute strace ../utils/smb2-ls "${TESTURL}/" 2>&1 >/d
 
 for IDX in `seq 1 $NUM_CALLS`; do
     echo -n "Testing prog_ls on root of share with socket failure at #${IDX} ..."
-    READV_CLOSE=${IDX} LD_PRELOAD=./ld_sockerr.so libtool --mode=execute valgrind --leak-check=full --show-leak-kinds=all --error-exitcode=1 ../utils/smb2-ls "${TESTURL}/" >/dev/null 2>valgrind.out || failure
+    READV_CLOSE=${IDX} LD_PRELOAD=./ld_sockerr.so libtool --mode=execute valgrind --leak-check=full --show-leak-kinds=all --error-exitcode=77 ../utils/smb2-ls "${TESTURL}/" >/dev/null 2>valgrind.out
+    expr $? "==" "77" >/dev/null && failure
     success
 done
 
