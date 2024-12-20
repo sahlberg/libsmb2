b0f456d61c08898fa92fd819449c3cc7c0ef2158
Ronnie Sahlberg <ronniesahlberg@gmail.com>
2024-12-21 02:21:20 +1000

    cleanup test_0103
    This test will be the blueprint for all additional tests of this type.
    Check if the application crashes and terminates by a signal
    or if there are memory leaks when malloc fails.
    
    Signed-off-by: Ronnie Sahlberg <ronniesahlberg@gmail.com>

--- tests/test_0103_ls_basic_valgrind_malloc_error.sh
+++ tests/test_0103_ls_basic_valgrind_malloc_error.sh
@@ -9,11 +9,16 @@ NUM_CALLS=`libtool --mode=execute ltrace -l \* ./prog_ls "${TESTURL}/" 2>&1 >/de
 echo "Num" $NUM_CALLS
 
 for IDX in `seq 1 $NUM_CALLS`; do
-    echo -n "Testing prog_ls on root of share with socket failure at #${IDX} ..."
-    #ALLOC_FAIL=${IDX} libtool --mode=execute valgrind --leak-check=full --show-leak-kinds=all --error-exitcode=77 ./prog_ls "${TESTURL}/" # >/dev/null 2>valgrind.out
-    #expr $? "==" "77" >/dev/null && failure
-    ALLOC_FAIL=${IDX} ./prog_ls "${TESTURL}/" 2>&1 # >/dev/null 2>valgrind.out
-    echo $?
+    # check for failing due to a signal (segv/abrt/...)
+    echo -n "Testing prog_ls for crashes at #${IDX} ..."
+    ALLOC_FAIL=${IDX} ./prog_ls "${TESTURL}/" 2>&1 >/dev/null 2>valgrind.out
+    expr $? ">=" "128" >/dev/null && failure
+    success
+
+    # check for memory leaks in error paths
+    echo -n "Testing prog_ls for memory leaks at #${IDX} ..."
+    ALLOC_FAIL=${IDX} libtool --mode=execute valgrind --leak-check=full --show-leak-kinds=all --error-exitcode=77 ./prog_ls "${TESTURL}/" >/dev/null 2>valgrind.out
+    expr $? "==" "77" >/dev/null && failure
     success
 done
 
