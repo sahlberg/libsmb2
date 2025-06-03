8f7a099947d405513df3473627969f37de0bd2e1
Pino Toscano <toscano.pino@tiscali.it>
2025-06-03 07:19:07 +0200

    Adapt portable-endian.h for GNU/Hurd
    Use <endian.h> & <byteswap.h>, as available from GNU libc.
    
    Signed-off-by: Pino Toscano <toscano.pino@tiscali.it>

--- include/portable-endian.h
+++ include/portable-endian.h
@@ -56,12 +56,12 @@
 #define htole64(x) (x)
 #define le64toh(x) (x)
 
-#elif defined(__linux__) || defined(__CYGWIN__) || defined(ESP_PLATFORM) || defined(__NetBSD__) || defined(__FreeBSD__) || defined(__DragonFly__) || defined(__OpenBSD__)
+#elif defined(__linux__) || defined(__CYGWIN__) || defined(ESP_PLATFORM) || defined(__NetBSD__) || defined(__FreeBSD__) || defined(__DragonFly__) || defined(__OpenBSD__) || defined(__GNU__)
 
-#if defined(__linux__) || defined(__CYGWIN__) || defined(PS4_PLATFORM) || defined(ESP_PLATFORM)
+#if defined(__linux__) || defined(__CYGWIN__) || defined(PS4_PLATFORM) || defined(ESP_PLATFORM) || defined(__GNU__)
 #include <endian.h>
 /* Include byteswap.h on linux since it might not be automatically included in some cases (e.g. alpine / musl) */
-#if defined(__linux__)
+#if defined(__linux__) || defined(__GLIBC__)
 #include <byteswap.h>
 #endif
 #else
