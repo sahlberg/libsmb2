bd78e9327d41b62548bf44e0ce2776b23afa1f5c
Ronnie Sahlberg <ronniesahlberg@gmail.com>
2025-08-03 11:19:30 +1000

    Make usmb2.o smaller.
    It now compiles down into an object file 1768 bytes in size.
    (gcc -Os on x86_64)
    
    Signed-off-by: Ronnie Sahlberg <ronniesahlberg@gmail.com>

--- examples/usmb2.c
+++ examples/usmb2.c
@@ -22,43 +22,126 @@
 #define CMD_WRITE    9
 #define CMD_GETINFO 16
 
-static void usmb2_build_hdr(struct usmb2_context *usmb2, int command)
+static int usmb2_build_request(struct usmb2_context *usmb2, int command,
+                               uint32_t struct_size, uint32_t length,
+                               uint64_t offset, uint8_t *fid, int fid_extra_offset,
+                               int spl,
+                               uint8_t *outdata, int outcount,
+                               int incount)
 {
-        uint64_t u64;
-        uint32_t u32;
-        uint8_t *buf = &usmb2->buf[4];
+        int len;
+        uint8_t *buf = &usmb2->buf[0];
+        uint32_t status;
 
+        memset(usmb2->buf, 0, sizeof(usmb2->buf));
+        /*
+         * SPL
+         */
+        *(uint32_t *)&usmb2->buf[0] = htobe32(spl);
+        buf += 4;
+        
+        /*
+         * SMB2 header
+         */
+       
         /* signature */
-        u32 = htole32(0x424d53fe);
-        *(uint32_t *)buf = u32;
+        *(uint32_t *)buf = htole32(0x424d53fe);
         buf += 4;
 
         /* header length (16 bits) + credit charge (16 bits)
          * Credit charge in smb2 is 1 per 64kb requested but we never read > 2048 bytes
          * so we can hardcode it to 1.
          */
-        u32 = htole32(0x00010040);
-        *(uint32_t *)buf = u32;
+        *(uint32_t *)buf = htole32(0x00010040);
         buf += 8; /* status, 4 bytes, is zero */
         
         /* command + credit request */
-        u32 = htole32(0x00010000 + command);
-        *(uint32_t *)buf = u32;
+        *(uint32_t *)buf = htole32(0x00010000 + command);
         buf += 12; /* flags and next command are both 0, 8 bytes */
 
         /* message id */
-        u64 = htole64(usmb2->message_id++);
-        *(uint64_t *)buf = u64;
+        *(uint64_t *)buf = htole64(usmb2->message_id++);
         buf += 12; /* 4 extra reserved bytes */
 
         /* tree id */
-        u32 = htole32(usmb2->tree_id);
-        *(uint32_t *)buf = u32;
+        *(uint32_t *)buf = htole32(usmb2->tree_id);
         buf += 4;
 
         /* session id */
-        u64 = htole64(usmb2->session_id);
-        *(uint64_t *)buf = u64;
+        *(uint64_t *)buf = htole64(usmb2->session_id);
+        buf += 24; /* 16 byte signature is all zero */
+
+        /*
+         * Command header
+         */
+        /* struct size (16 bits) + FILE_INFO + SMB2_FILE_STANDARD_INFO */
+        *(uint32_t *)buf = htole32(struct_size);
+        buf += 4;
+        
+        /* length */
+        *(uint32_t *)buf = htole32(length);
+        buf += 4;
+
+        /* offset */
+        *(uint64_t *)buf = htole64(offset);
+        buf += 8 + fid_extra_offset;
+
+        /* fid. fid is stored 8 bytes further into the pdu for getinfo vs read/write */
+        memcpy(buf, fid, 16);
+
+
+        /*
+         * Write the request to the socket
+         */
+        buf = &usmb2->buf[0];
+        spl = spl + 4;
+        while (spl) {
+                len = write(usmb2->fd, buf, spl);
+                if (len < 0) {
+                        return -1;
+                }
+                spl -= len;
+                buf += len;
+        }
+
+
+        /*
+         * Write payload data to the socket
+         */
+        if (outdata) {
+                while (outcount) {
+                        len = write(usmb2->fd, outdata, outcount);
+                        if (len < 0) {
+                                return -1;
+                        }
+                        outcount -= len;
+                        outdata += len;
+                }
+        }
+
+
+        /*
+         * Read SPL, SMB2 header and command reply header (and file info blob) from socket
+         */
+        buf = &usmb2->buf[0];
+        while (incount) {
+                len = read(usmb2->fd, buf, incount);
+                if (len < 0) {
+                        continue;
+                }
+                incount -= len;
+                buf += len;
+        }
+
+        /*
+         * Status, fail hard if not successful.  Might need to add a check for pending here.
+         */
+        status = le32toh(*(uint32_t *)&usmb2->buf[4 + 8]);
+        if (status) {
+                return -1;
+        }
+        
+        return 0;
 }
 
 /* READ in units of 512 bytes */
@@ -66,70 +149,14 @@ int usmb2_pread(struct usmb2_context *usmb2, uint8_t *fid, uint8_t *buf, int cou
 {
         int spl, len;
         uint32_t u32;
-        uint64_t u64;
-        uint8_t *hdr = &usmb2->buf[4 + 64];
 
         count  *= 512;
         offset *= 512;
         
-        memset(usmb2->buf, 0, sizeof(usmb2->buf));
-        usmb2_build_hdr(usmb2, CMD_READ);
-
-        /* struct size (16 bits) + padding (8 bits) + flags (8 bits) */
-        u32 = htole32(0x00000031);
-        *(uint32_t *)hdr = u32;
-        hdr += 4;
-
-        /* length */
-        u32 = htole32(count);
-        *(uint32_t *)hdr = u32;
-        hdr += 4;
-
-        /* offset */
-        u64 = htole64(offset);
-        *(uint64_t *)hdr = u64;
-        hdr += 8;
-
-        /* file id */
-        memcpy(hdr, fid, 16);
-        hdr += 16;
-
-        /* spl */
-        spl = 64 + 48 + 8;
-        u32 = htobe32(spl);
-        *(uint32_t *)usmb2->buf = u32;
-
-
-        /* Write the request to the socket */
-        hdr = &usmb2->buf[0];
-        spl = spl + 4;
-        while (spl) {
-                len = write(usmb2->fd, hdr, spl);
-                if (len < 0) {
-                        return -1;
-                }
-                spl -= len;
-                hdr += len;
-        }
-
-        /* Read SPL, SMB2 header and read reply header from socket */
-        spl = 4 + 64 + 16;
-        hdr = &usmb2->buf[0];
-        while (spl) {
-                len = read(usmb2->fd, hdr, spl);
-                if (len < 0) {
-                        continue;
-                }
-                spl -= len;
-                hdr += len;
-        }
-
-        spl = be32toh(*(uint32_t *)&usmb2->buf[0]);
-        
-        /* status, fail hard if not successful */
-        u32 = le32toh(*(uint32_t *)&usmb2->buf[4 + 8]);
-        if (u32) {
-                return -1;
+        if (usmb2_build_request(usmb2, CMD_READ, 0x00000031, count, offset, fid, 0,
+                            64 + 48 + 8, NULL, 0,
+                                4 + 64 + 16)) {
+                   return -1;
         }
 
         /* number of bytes returned */
@@ -153,155 +180,27 @@ int usmb2_pread(struct usmb2_context *usmb2, uint8_t *fid, uint8_t *buf, int cou
 /* WRITE in units of 512 bytes */
 int usmb2_pwrite(struct usmb2_context *usmb2, uint8_t *fid, uint8_t *buf, int count, int offset)
 {
-        int spl, len;
-        uint32_t u32;
-        uint64_t u64;
-        uint8_t *hdr = &usmb2->buf[4 + 64];
-
         count  *= 512;
         offset *= 512;
         
-        memset(usmb2->buf, 0, sizeof(usmb2->buf));
-        usmb2_build_hdr(usmb2, CMD_WRITE);
-
-        
-        /* struct size (16 bits) + data offset (16 bits) */
-        u32 = htole32(0x00700031);
-        *(uint32_t *)hdr = u32;
-        hdr += 4;
-
-        /* length */
-        u32 = htole32(count);
-        *(uint32_t *)hdr = u32;
-        hdr += 4;
-
-        /* offset */
-        u64 = htole64(offset);
-        *(uint64_t *)hdr = u64;
-        hdr += 8;
-
-        /* file id */
-        memcpy(hdr, fid, 16);
-        hdr += 16;
-
-        /* spl */
-        spl = 64 + 48 + count;
-        u32 = htobe32(spl);
-        *(uint32_t *)usmb2->buf = u32;
-
-
-        /* Write the request to the socket */
-        hdr = &usmb2->buf[0];
-        spl = 64 + 48 + 4;
-        while (spl) {
-                len = write(usmb2->fd, hdr, spl);
-                if (len < 0) {
-                        return -1;
-                }
-                spl -= len;
-                hdr += len;
-        }
-
-        /* Write the data to the socket */
-        while (count) {
-                len = write(usmb2->fd, buf, count);
-                if (len < 0) {
-                        return -1;
-                }
-                count -= len;
-                buf += len;
-        }
-        
-        /* Read SPL, SMB2 header and read reply header from socket */
-        spl = 4 + 64 + 16;
-        hdr = &usmb2->buf[0];
-        while (spl) {
-                len = read(usmb2->fd, hdr, spl);
-                if (len < 0) {
-                        continue;
-                }
-                spl -= len;
-                hdr += len;
-        }
-
-        spl = be32toh(*(uint32_t *)&usmb2->buf[0]);
-        
-        /* status, fail hard if not successful */
-        u32 = le32toh(*(uint32_t *)&usmb2->buf[4 + 8]);
-        if (u32) {
-                return -1;
+        if (usmb2_build_request(usmb2, CMD_WRITE, 0x00700031, count, offset, fid, 0,
+                            64 + 48 + count, buf, count,
+                                4 + 64 + 16)) {
+                   return -1;
         }
 
         /* number of bytes returned */
-        u32 = le32toh(*(uint32_t *)&usmb2->buf[4 + 64 + 4]);
-
         /* We only read in 512 byte chunks so we will never need to worry about padding until next pdu*/
-        return u32 / 512;
+        return le32toh(*(uint32_t *)&usmb2->buf[4 + 64 + 4]) / 512;
 }
 
 /* return units of 512 bytes */
 int usmb2_size(struct usmb2_context *usmb2, uint8_t *fid)
 {
-        int spl, len;
-        uint32_t u32;
-        //uint64_t u64;
-        uint8_t *hdr = &usmb2->buf[4 + 64];
-
-        memset(usmb2->buf, 0, sizeof(usmb2->buf));
-        usmb2_build_hdr(usmb2, CMD_GETINFO);
-
-        /* struct size (16 bits) + FILE_INFO + SMB2_FILE_STANDARD_INFO */
-        u32 = htole32(0x05010029);
-        *(uint32_t *)hdr = u32;
-        hdr += 4;
-
-        /* output buffer length */
-        u32 = htole32(0x0000ffff);
-        *(uint32_t *)hdr = u32;
-        hdr += 4;
-        
-        /* input buffer offset */
-        u32 = htole32(0x00000068);
-        *(uint32_t *)hdr = u32;
-        hdr += 16; /* input size is zero, additional info is zero, flags are zero */
-
-        /* file id */
-        memcpy(hdr, fid, 16);
-
-
-        /* spl */
-        spl = 64 + 40;
-        u32 = htobe32(spl);
-        *(uint32_t *)usmb2->buf = u32;
-
-        /* Write the request to the socket */
-        hdr = &usmb2->buf[0];
-        spl = spl + 4;
-        while (spl) {
-                len = write(usmb2->fd, hdr, spl);
-                if (len < 0) {
-                        return -1;
-                }
-                spl -= len;
-                hdr += len;
-        }
-
-        /* Read SPL, SMB2 header and getinfo reply header and file info blob from socket */
-        spl = 4 + 64 + 8 + 24;
-        hdr = &usmb2->buf[0];
-        while (spl) {
-                len = read(usmb2->fd, hdr, spl);
-                if (len < 0) {
-                        continue;
-                }
-                spl -= len;
-                hdr += len;
-        }
-
-        /* status, fail hard if not successful */
-        u32 = le32toh(*(uint32_t *)&usmb2->buf[4 + 8]);
-        if (u32) {
-                return -1;
+        if (usmb2_build_request(usmb2, CMD_GETINFO, 0x05010029, 0x0000ffff, 0x00000068, fid, 8,
+                            64 + 40, NULL, 0,
+                                4 + 64 + 8 + 24)) {
+                   return -1;
         }
 
         /* We only read in 512 byte chunks so we will never need to worry about padding until next pdu*/
